5e942b762231ee78ea106f107195895941ebb13f
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-06-25 23:24:44 +0300

    Initial Commit

---
+++ Temperature Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Temperature Converter
+//
+//  Created by newbie on 25.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Temperature Converter/Temperature_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConverterApp.swift
+//  Temperature Converter
+//
+//  Created by newbie on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
